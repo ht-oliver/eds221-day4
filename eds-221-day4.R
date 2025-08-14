13c8a5fedb277f26aeab5176bfe5f2eda518d3c2
ht-oliver <holiver327@gmail.com>
2025-08-14 11:58:22 -0700

    basic func

--- eds-221-day4.R
+++ eds-221-day4.R
@@ -17,3 +17,36 @@ doublr <- function(x) {
 }
 
 doublr(4)
+
+# write a function with conditionals
+# example is converting animals' ages
+
+animal_age <- function(animal, age) {
+  if (animal == "dragonfly") {
+    print (age * 76.2)
+  } else if (animal == "tortoise") {
+    print (age * 0.7)
+  } else print("NO NO NO NO NO") # warning message
+  
+}
+
+animal_age("crump", -3)
+
+# write an updated version of the animal age funtiion with error messages
+animal_age_stop <- function(animal, age){
+  
+  if (!animal %n% c("dragonfly", "tortoise"))
+    stop("Oops! You made a mistake lil g, animal must be a dog or a goat.")
+}
+
+  if(is.numeric(age)==FALSE) {
+    stop("Lil g hahah my boi the age must be a number.")
+    
+  }
+  
+  if (age <= 0 | age > 50) {
+    warning("Lil g I suggest making absolutely sure your numbers make sense")
+  }
+
+animal_age_stop("frog", 0)
+
